2963fbbec49573d96cb71df19343151bf9f408a3
doyansu <90863007+doyansu@users.noreply.github.com>
2021-09-17 21:03:22 +0800

    Update e128(sqrt).c

--- e128(sqrt).c
+++ e128(sqrt).c
@@ -1,3 +1,12 @@
+/*
+耗很多空間的解法
+
+建立 mid[n] 為座標 (n , n) 的值
+利用 sqrt() 求出 n - 1 的平方根 r 可得知 r + 1 為目標座標(x , y)其一
+n - mid[r + 1] 為 n 秒與中間座標差 所以 (r + 1) - abs(n - mid[r + 1]) 即為另一座標
+再根據 r + 1 為奇、偶數判斷輸出是否交換位置
+*/
+
 #include <stdio.h>
 #include <math.h>
 #define SIZE 100001
