d7bd7802ef62b87f753c76017506b3f6470cc06a
yuan hao Fan <fyuanhao91@gmail.com>
2022-01-26 03:43:17 +0800

    update e995

--- C/e995.c
+++ C/e995.c
@@ -2,7 +2,7 @@
 //參考 https://home.gamer.com.tw/creationDetail.php?sn=4774857
 //先找到 n 對應數字的長度，再找到該數字，在找到是第幾位
 
-long long int solve(long long int index){
+int solve(long long int index){
     static long long int numOfDigit[10] = {0, 9, 189, 2889, 38889, 488889, 5888889, 68888889, 788888889, 8888888889},
                            start[10] = {0, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
     long long int digit;
@@ -19,6 +19,6 @@ long long int solve(long long int index){
 int main(){
     long long int n;
     while(scanf("%lld", &n) == 1)
-        printf("%lld\n", solve(n));
+        printf("%d\n", solve(n));
     return 0;
 }
